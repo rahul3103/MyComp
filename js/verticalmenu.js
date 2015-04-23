7e250eba3509bff8d6a9237905cb972659033b44
Brijesh Bittu <brijeshb42@gmail.com>
2015-04-23 23:21:05 +0530

    Update verticalmenu.js

--- js/verticalmenu.js
+++ js/verticalmenu.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 			
 			var accordion_head = $('.accordion > li > a'),
 				accordion_body = $('.accordion li > .sub-menu'),
-				accordion_body_a = $('.accordion > li > .sub-menu > li > a'),
-				accordion_sub_body = $('.accordion li > .sub-menu > .sub-sub-menu');
+				accordion_body_a = $('.accordion .sub-menu >li>a'),
+				accordion_sub_body = $('.sub-sub-menu');
 
 			// Open the first tab on load
 
@@ -20,14 +20,20 @@ $(document).ready(function() {
 				event.preventDefault();
 
 				// Show and hide the tabs on click
+                var $this = $(this);
 
 				if ($(this).attr('class') != 'active'){
 					accordion_body.slideUp('normal');
 					$(this).next().stop(true,true).slideToggle('normal');
 					accordion_head.removeClass('active');
 					$(this).addClass('active');
+                    accordion_sub_body.slideUp();
 
-}
+                }else {
+                    accordion_body.slideUp(function() {
+                        $this.removeClass('active');
+                    });
+                }
 				
 });
 				accordion_body_a.on('click', function(event) {
@@ -37,14 +43,18 @@ $(document).ready(function() {
 				event.preventDefault();
 
 				// Show and hide the tabs on click
-
+                var $this = $(this);
 
 				if ($(this).attr('class') != 'active'){
 					accordion_sub_body.slideUp('normal');
 					$(this).next().stop(true,true).slideToggle('normal');
 					accordion_body_a.removeClass('active');
 					$(this).addClass('active');
-				}
+				}else {
+                    accordion_sub_body.slideUp(function() {
+                        $this.removeClass('active');
+                    });
+                }
 			});
 
 		});
