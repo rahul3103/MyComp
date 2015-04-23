66d2393eae02b04956edb1e93c202222d564484a
Rahul Shrivastava <rahul3103@gmail.com>
2015-04-23 19:57:50 +0530

    Commit 2
    Sub Sub Menu

--- js/verticalmenu.js
+++ js/verticalmenu.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 			// Store variables
 			
 			var accordion_head = $('.accordion > li > a'),
-				accordion_body = $('.accordion li > .sub-menu');
+				accordion_body = $('.accordion li > .sub-menu'),
+				accordion_body_a = $('.accordion > li > .sub-menu > li > a'),
+				accordion_sub_body = $('.accordion li > .sub-menu > .sub-sub-menu');
 
 			// Open the first tab on load
 
@@ -24,8 +26,25 @@ $(document).ready(function() {
 					$(this).next().stop(true,true).slideToggle('normal');
 					accordion_head.removeClass('active');
 					$(this).addClass('active');
-				}
 
+}
+				
+});
+				accordion_body_a.on('click', function(event) {
+
+				// Disable header links
+				
+				event.preventDefault();
+
+				// Show and hide the tabs on click
+
+
+				if ($(this).attr('class') != 'active'){
+					accordion_sub_body.slideUp('normal');
+					$(this).next().stop(true,true).slideToggle('normal');
+					accordion_body_a.removeClass('active');
+					$(this).addClass('active');
+				}
 			});
 
 		});
