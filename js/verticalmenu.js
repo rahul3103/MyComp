a4f626562ba529805e0608e10f6f84c49bae248d
Rahul Shrivastava <rahul3103@gmail.com>
2015-04-24 12:10:54 +0530

    Commit 5
    Menu colors and soma js

--- js/verticalmenu.js
+++ js/verticalmenu.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
 			var accordion_head = $('.accordion > li > a'),
 				accordion_body = $('.accordion li > .sub-menu'),
 				accordion_body_a = $('.accordion .sub-menu >li>a'),
-				accordion_sub_body = $('.sub-sub-menu');
+				accordion_sub_body = $('.sub-sub-menu'),
+				accordion_sub_body_a = $('.accordion .sub-menu .sub-sub-menu > li > a');
 
 			// Open the first tab on load
 
@@ -49,6 +50,7 @@ $(document).ready(function() {
 					accordion_sub_body.slideUp('normal');
 					$(this).next().stop(true,true).slideToggle('normal');
 					accordion_body_a.removeClass('active');
+					accordion_sub_body_a.removeClass('active');
 					$(this).addClass('active');
 				}else {
                     accordion_sub_body.slideUp(function() {
@@ -57,6 +59,19 @@ $(document).ready(function() {
                 }
 			});
 
+				accordion_sub_body_a.on('click', function(event) {
+				var $this = $(this);
+				if ($(this).attr('class') != 'active'){
+					
+					$(this).next().stop(true,true).slideToggle('normal');
+					
+					$(this).addClass('active');
+				}else {
+                    accordion_sub_body.slideUp(function() {
+                        $this.removeClass('active');
+                    });
+                }
+			});
 		});
 
 
